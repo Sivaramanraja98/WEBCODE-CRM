67968e73d7266fb1e2601c029a2888075595d676
SIVARAMAN ALIAS RAJA S <127581183+Sivaramanraja98@users.noreply.github.com>
2023-12-07 12:31:04 +0530

    Add files via upload

--- DB/connection.js
+++ DB/connection.js
@@ -1,13 +1,15 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-
-exports.db = ()=>{
-mongoose.connect(`mongodb+srv://SivaSSR:Siva2298@cluster.garse2f.mongodb.net/?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(()=>{
-console.log('Connection established');
-}).catch((err)=>{
-    console.log(err);
-})
+exports.db = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connection established");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
